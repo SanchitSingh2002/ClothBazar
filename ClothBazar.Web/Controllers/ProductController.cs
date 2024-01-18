b7deb2cf177824f010fc87df4a94e4ceef3dc6cf
sanchit singh <sanchitsingh56149@gmail.com>
2024-01-18 19:19:40 +0530

    CRUD Operations on Product and Category Completed

---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+
+        // GET: Prouduct
+        public ActionResult Index()
+        {
+            return View(); 
+        }
+
+        public ActionResult ProductTable(string Search)
+        {
+            var products = productsService.GetProducts();
+
+            if(string.IsNullOrEmpty(Search)  == false)
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(Search.ToLower())).ToList();  //LINQ
+            }
+
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var product = productsService.GetProduct(ID);
+            return PartialView(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            productsService.UpdateProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int ID)
+        {
+            productsService.DeleteProduct(ID);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
